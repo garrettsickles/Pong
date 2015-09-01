fecd0507049c368b91ddf46cf9a405c80dbbe957
Jacob Adelgren <jadelgre@mymail.mines.edu>
2015-09-01 14:59:35 -0600

    right paddle follows ball

--- js/main.js
+++ js/main.js
@@ -21,38 +21,48 @@ var pongBall = {
 }
 
 var leftPaddle = { 
-  x: 10,
+  x: 20,
   y: canvasHeight / 2
 }
 
 var rightPaddle = {
-  x: canvasWidth - 10,
+  x: canvasWidth - 20,
   y: canvasHeight / 2
 }
 
 var draw = function () {
   // clear the board
   context.clearRect(0,0,canvasWidth, canvasHeight);
-
-  // draw paddles and ball
-  
-
-  // draw ball
+  // initialize graphics properties
   context.beginPath();
   context.strokeStyle = "white";
+
+  // draw paddles and ball
+  var height = 100;
+  var width = 10;
+
+  context.rect(rightPaddle.x, rightPaddle.y-height/2, width, height);
+  context.stroke();
+  context.closePath();
+  // draw ball
+  context.beginPath();
   context.arc(pongBall.x,pongBall.y,10,0,2*Math.PI);
   context.stroke();
+  context.closePath();
 }
 
 var update = function () {
   // update left paddle
 
   // update right paddle
-
+  rightPaddle.y += pongBall.dy; 
+  
   // move ball
   pongBall.x += pongBall.dx;
   pongBall.y += pongBall.dy;
-  if( pongBall.x + pongBall.rad >= canvasWidth || pongBall.x - pongBall.rad <= 0 ) pongBall.dx = -pongBall.dx;
+  if( pongBall.x + pongBall.rad >= canvasWidth
+    || pongBall.x - pongBall.rad <= 0 
+    || pongBall.x + pongBall.rad >= rightPaddle.x ) pongBall.dx = -pongBall.dx;
   if( pongBall.y + pongBall.rad >= canvasHeight || pongBall.y - pongBall.rad <= 0 ) pongBall.dy = -pongBall.dy;
   // redraw
   draw();
