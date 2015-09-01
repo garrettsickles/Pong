2c228a6e305f36d50bd40bf52e04de6dbbba95b4
Jacob Adelgren <jadelgre@mymail.mines.edu>
2015-09-01 13:41:22 -0600

    created empty css file, added canvas for game

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+var canvas;
+var context; 
 
+var initialize_canvas = function() {
+  canvas = document.getElementById("game");
+  canvas.style.backgroundColor = "black";
+  canvas.width = 500;
+  canvas.height = 500;
+  context = canvas.getContext('2d');
+} 
+  
+
+window.onload = function() {
+  initialize_canvas();
+}
