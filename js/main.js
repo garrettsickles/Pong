1e65b31b29ed6aa186bd609b36592a86e22bfaf8
Jacob Adelgren <jadelgre@mymail.mines.edu>
2015-09-01 14:41:24 -0600

    added ball and skeleton methods

--- js/main.js
+++ js/main.js
@@ -1,15 +1,51 @@
 var canvas;
+var fps = 30;
 var context; 
+var canvasHeight = 500;
+var canvasWidth = 500;
 
 var initialize_canvas = function() {
   canvas = document.getElementById("game");
   canvas.style.backgroundColor = "black";
-  canvas.width = 500;
-  canvas.height = 500;
+  canvas.width = canvasWidth;
+  canvas.height = canvasWidth;
   context = canvas.getContext('2d');
 } 
   
+var pongBall = {
+  dx: 0,
+  dy: 0,
+  x: canvasWidth / 2,
+  y: canvasHeight / 2
+}
+
+var leftPaddle = { 
+  x: 10,
+  y: canvasHeight / 2
+}
+
+var rightPaddle = {
+  x: canvasWidth - 10,
+  y: canvasHeight / 2
+}
+
+var draw = function () {
+  // draw paddles and ball
+  
+
+  // draw ball
+  context.beginPath();
+  context.strokeStyle = "white";
+  context.arc(pongBall.x,pongBall.y,10,0,2*Math.PI);
+  context.stroke();
+}
+
+var update = function () {
+  // check logic
+  draw();
+}
 
 window.onload = function() {
   initialize_canvas();
+  setInterval(update, 1000 / fps );
 }
