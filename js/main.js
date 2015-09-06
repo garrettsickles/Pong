f5d551979d26039e5652ac14db02d47fbad223a3
Garrett Frank Sickles <garrettsickles@gmail.com>
2015-09-06 16:43:24 -0600

    Left paddle now working with key controls smoothly.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,45 @@
 var canvas;
 var fps = 60;
+
+var paddleSpeed = 5;
+var maxPaddleSpeed = 50;
+
+var mover;
+var moverLatency = 10;
+
 var context; 
 var canvasHeight = 500;
 var canvasWidth = 500;
 
-var initialize_canvas = function() {
+var initializeControls = function() {
+  document.onkeydown = function(e) {
+    if (!mover) {
+      if (e.keyCode == 38) {
+        mover = setInterval(function() {leftPaddle.y -= paddleSpeed;}, moverLatency); 
+      } else if (e.keyCode == 40) {
+        mover = setInterval(function() {leftPaddle.y += paddleSpeed;}, moverLatency); 
+      }
+    }
+  };
+
+  document.onkeyup = function(e) {
+    if (mover) {
+      if (e.keyCode == 38 || e.keyCode == 40) {
+        clearInterval(mover); 
+        mover = null;
+      }
+    }
+  }
+}
+
+var initializeCanvas = function() {
   canvas = document.getElementById("game");
   canvas.style.backgroundColor = "black";
   canvas.width = canvasWidth;
   canvas.height = canvasWidth;
   context = canvas.getContext('2d');
-} 
-  
+}
+
 var pongBall = {
   dx: -10,
   dy: -1,
@@ -36,22 +64,31 @@ var rightPaddle = {
 
 var draw = function () {
   // clear the board
-  context.clearRect(0,0,canvasWidth, canvasHeight);
+  context.clearRect(0, 0, canvasWidth, canvasHeight);
+
   // initialize graphics properties
   context.beginPath();
   context.strokeStyle = "white";
 
   // draw paddles and ball
   context.rect(leftPaddle.x, leftPaddle.y-leftPaddle.height/2, leftPaddle.width, leftPaddle.height);
+  context.fillStyle = 'white';
+  context.fill();
   context.stroke();
   context.closePath();
+
   // draw right paddle
   context.rect(rightPaddle.x, rightPaddle.y-rightPaddle.height/2, rightPaddle.width, rightPaddle.height);
+  context.fillStyle = 'white';
+  context.fill();
   context.stroke();
   context.closePath();
+
   // draw ball
   context.beginPath();
-  context.arc(pongBall.x,pongBall.y,10,0,2*Math.PI);
+  context.arc(pongBall.x, pongBall.y, pongBall.rad, 0, 2*Math.PI);
+  context.fillStyle = 'white';
+  context.fill();
   context.stroke();
   context.closePath();
 }
@@ -60,7 +97,7 @@ var update = function () {
   // update left paddle
 
   // update right paddle
-  if( rightPaddle.y + rightPaddle.height / 2 + pongBall.dy < canvasHeight && rightPaddle.y - rightPaddle.height / 2 + pongBall.dy > 0 ) rightPaddle.y += pongBall.dy; 
+  if ( rightPaddle.y + rightPaddle.height / 2 + pongBall.dy < canvasHeight && rightPaddle.y - rightPaddle.height / 2 + pongBall.dy > 0 ) rightPaddle.y += pongBall.dy; 
   
   // move ball
   pongBall.x += pongBall.dx;
@@ -69,14 +106,19 @@ var update = function () {
     || pongBall.x - pongBall.rad <= 0 
     || pongBall.x + pongBall.rad >= rightPaddle.x 
     || (pongBall.x - pongBall.rad <= leftPaddle.x+leftPaddle.width 
-      && pongBall.y < leftPaddle.y + leftPaddle.height/2
-      && pongBall.y > leftPaddle.y - leftPaddle.height/2)) pongBall.dx = -pongBall.dx;
+      && pongBall.y < leftPaddle.y + leftPaddle.height/2 + pongBall.rad
+      && pongBall.y > leftPaddle.y - leftPaddle.height/2 - pongBall.rad))
+  {
+    pongBall.dx = -pongBall.dx;
+    pongBall.dy = -1.0 * pongBall.dy;
+  }
   if( pongBall.y + pongBall.rad >= canvasHeight || pongBall.y - pongBall.rad <= 0 ) pongBall.dy = -pongBall.dy;
   // redraw
   draw();
 }
 
 window.onload = function() {
-  initialize_canvas();
+  initializeControls();
+  initializeCanvas();
   setInterval(update, 1000 / fps );
 }
